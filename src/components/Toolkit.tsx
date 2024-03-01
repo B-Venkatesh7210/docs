6a030e562feb354deb7de529c2427f595191ade3
Venkatesh-Router <67518179+B-Venkatesh7210@users.noreply.github.com>
2024-03-01 16:58:08 +0530

    Adding Intialisation

--- src/components/Toolkit.tsx
+++ src/components/Toolkit.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import magicLogo from "../logos/magicLogo.jpg";
 import particleLogo from "../logos/particleLogo.png";
+import { CopyBlock, dracula, vs2015 } from "react-code-blocks";
 
 interface Button {
   id: string;
   icon: string;
   label: string;
   importData: string;
+  initialization: string;
 }
 
 const Toolkit = () => {
@@ -16,66 +18,144 @@ const Toolkit = () => {
       icon: magicLogo,
       label: "Magic",
       importData: `import magic from "magic"`,
+      initialization: `
+      
+// Initialize the Magic instance
+export const magic = new Magic("YOUR_API_KEY", {
+  network: {
+    rpcUrl: "",
+    chainId: 11155111, // or preferred chain
+  },
+});
+
+      
+const connect = async () => {
+  try {
+    await magic.wallet.connectWithUI();
+    const web3Provider = new ethers.providers.Web3Provider(
+      magic.rpcProvider,
+      "any"
+    );
+      
+    const smartAccount = await createSmartAccountClient({
+      signer: web3Provider.getSigner() as LightSigner,
+      bundlerUrl: "", // <-- Read about this at https://docs.biconomy.io/dashboard#bundler-url
+      biconomyPaymasterApiKey: "", // <-- Read about at https://docs.biconomy.io/dashboard/paymaster
+      rpcUrl: "" // <-- read about this at https://docs.biconomy.io/Account/methods#createsmartaccountclient
+    });
+      
+    const address = await smartAccount.getAccountAddress();
+  } catch (error) {
+    console.error(error);
+  }
+};
+      `,
     },
     {
       id: "particle",
       icon: particleLogo,
       label: "Particle",
       importData: `import magic from "particle"`,
+      initialization: `
+      
+// Initialize the Particle instance
+const particle = new ParticleAuthModule.ParticleNetwork({
+  projectId: "",
+  clientKey: "",
+  appId: "",
+  wallet: {
+    displayWalletEntry: true,
+    defaultWalletEntryPosition: ParticleAuthModule.WalletEntryPosition.BR,
+  },
+});
+
+
+const connect = async () => {
+  try {
+    const userInfo = await particle.auth.login();
+    console.log("Logged in user:", userInfo);
+    const particleProvider = new ParticleProvider(particle.auth);
+    const web3Provider = new ethers.providers.Web3Provider(
+      particleProvider,
+      "any"
+    );
+      
+    const smartAccount = await createSmartAccountClient({
+      signer: web3Provider.getSigner() as LightSigner,
+      bundlerUrl: "", // <-- Read about this at https://docs.biconomy.io/dashboard#bundler-url
+      biconomyPaymasterApiKey: "", // <-- Read about at https://docs.biconomy.io/dashboard/paymaster
+      rpcUrl: "" // <-- read about this at https://docs.biconomy.io/Account/methods#createsmartaccountclient
+    });
+      
+    const address = await smartAccount.getAccountAddress();
+  } catch (error) {
+    console.error(error);
+  }
+};
+      `,
     },
     {
       id: "dynamic",
       icon: magicLogo,
       label: "Dynamic",
       importData: `import magic from "dynamic"`,
+      initialization: `// Initialize the Dynamic instance`,
     },
     {
       id: "privy",
       icon: magicLogo,
       label: "Privy",
       importData: `import magic from "privy"`,
+      initialization: `// Initialize the Privy instance`,
     },
     {
       id: "dfns",
       icon: magicLogo,
       label: "DFNS",
       importData: `import magic from "dfns"`,
+      initialization: `// Initialize the DFNS instance`,
     },
     {
       id: "capsule",
       icon: magicLogo,
       label: "Capsule",
       importData: `import magic from "capsule"`,
+      initialization: `// Initialize the Capsule instance`,
     },
     {
       id: "turnkey",
       icon: magicLogo,
       label: "Turnkey",
       importData: `import magic from "turnkey"`,
+      initialization: `// Initialize the Turnkey instance`,
     },
     {
       id: "web3auth",
       icon: magicLogo,
       label: "Web3Auth",
       importData: `import magic from "web3auth"`,
+      initialization: `// Initialize the Web3Auth instance`,
     },
     {
       id: "ethers",
       icon: magicLogo,
       label: "Ethers",
       importData: `import magic from "ethers"`,
+      initialization: `// Initialize the Ethers instance`,
     },
     {
       id: "viem",
       icon: magicLogo,
       label: "Viem",
       importData: `import magic from "viem"`,
+      initialization: `// Initialize the Viem instance`,
     },
     {
       id: "wagmi",
       icon: magicLogo,
       label: "Wagmi",
       importData: `import magic from "wagmi"`,
+      initialization: `// Initialize the Wagmi instance`,
     },
   ];
 
@@ -85,18 +165,21 @@ const Toolkit = () => {
       icon: magicLogo,
       label: "Sponsored",
       importData: `import sponsored from "sponsored"`,
+      initialization: `// Initialize the Sponsored instance`,
     },
     {
       id: "erc20",
       icon: magicLogo,
       label: "ERC20",
       importData: `import erc20 from "erc20"`,
+      initialization: `// Initialize the ERC20 instance`,
     },
     {
       id: "userPaid",
       icon: magicLogo,
       label: "User Paid",
       importData: `import userPaid from "userPaid"`,
+      initialization: `// Initialize the User Paid instance`,
     },
   ];
 
@@ -107,15 +190,25 @@ const Toolkit = () => {
     transactionButtons[0]
   );
   const [imports, setImports] = useState("");
+  const [initializationCode, setInitializationCode] = useState("");
 
   useEffect(() => {
     updateImports([activeLoginType, transactionType]);
+    updateInitializationCode([activeLoginType, transactionType]);
   }, [activeLoginType, transactionType]);
 
   const updateImports = (buttons: Button[]) => {
     const newImport = buttons.map((button) => button.importData).join("\n");
     setImports(newImport);
     console.log("New Imports final", newImport);
+  };
+
+  const updateInitializationCode = (buttons: Button[]) => {
+    const newInitializationCode = buttons
+      .map((button) => button.initialization)
+      .join("\n");
+    setInitializationCode(newInitializationCode);
+    console.log("New Initialization Code final", newInitializationCode);
   };
 
   const handleLoginButtonClick = (button: Button) => {
@@ -178,6 +271,15 @@ const Toolkit = () => {
       <div className="flex flex-col justify-start items-start w-[65%] h-[30rem] py-2 px-4">
         Code
         <div className="flex flex-col bg-[#131417] border-solid border-[1px] border-opacity-60 border-gray-500 justify-start items-start w-full h-full mt-2 rounded-md overflow-y-auto overflow-x-auto py-2 px-4">
+          <div className="w-full h-full">
+            {" "}
+            <CopyBlock
+              text={imports + initializationCode}
+              language="typescript"
+              theme={vs2015}
+              customStyle={{ backgroundColor: "#131417" }}
+            />
+          </div>
         </div>
       </div>
     </div>
